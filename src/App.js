3bc21eea1c4baacd8229b1e850e30782180697f7
noor <noormedia82@gmail.com>
2019-11-07 11:58:47 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 function App() {
   const [term, setTerm] = useState("");
   const [pictures, setPictures] = useState([]);
   const [count, setCount] = useState(1);
+  const [prevRefresh, setPrevRefresh] = useState(false);
   const changeHandler = e => {
+    e.preventDefault();
     setTerm(e.target.value);
     console.log(term);
   };
+  // ..........
+  // after changing the component
+  useEffect(() => {
+    prevRefresh &&
+      axios
+        .get("https://api.unsplash.com/search/photos", {
+          params: {
+            query: term,
+            page: count,
+            per_page: 12
+          },
+          headers: {
+            Authorization:
+              "Client-ID 89a74f8c26da940b295f7c22ccaf83e3404ac033065c8db15fcbbc3b0639a400"
+          }
+        })
+        .then(response => {
+          setPictures([...response.data.results]);
+        })
+        .catch(error => {
+          console.log(error.message);
+        });
+  });
 
+  // ..........
+  // send first request
   const sendRequest = e => {
     e.preventDefault();
     axios
@@ -17,7 +44,7 @@ function App() {
         params: {
           query: term,
           page: count,
-          per_page: 9
+          per_page: 12
         },
         headers: {
           Authorization:
@@ -32,31 +59,27 @@ function App() {
       });
     console.log(count);
   };
-  const increment = () => {
-    setCount(count + 1);
-    console.log("clicked");
+
+  // next prev page
+
+  const nextPrevPage = e => {
+    e.preventDefault();
+    e.target.name === "increment"
+      ? setCount(count + 1)
+      : e.target.name === "decrement" && count > 1 && setCount(count - 1);
+    setPrevRefresh(true);
+    console.log("clicked", count);
   };
-  // const decrement = () => {
-  //   setCount(count - 1);
-  //   console.log("clicked");
-  // };
+
   return (
     <div className="container-fluid">
       <div className="row text-center text-white bg-dark">
         <div className="col-12 p-2 mt-3">
-          <h1>Welcome to the Gallery App</h1>
+          <h1>Responsive Gallery App</h1>
         </div>
       </div>
-      <div className="row text-center">
-        <div className="col-12 mt-2">
-          <button className="btn btn-primary" onClick={increment}>
-            Next Page
-          </button>
-          <h3>
-            <span className="badge text-white bg-warning">Page :{count}</span>
-          </h3>
-        </div>
-      </div>
+
+      {/*  */}
       <div className="row justify-content-center mt-5">
         <div className="col-sm-12 col-md-8 col-lg-6">
           <form onSubmit={sendRequest} className="form-group">
@@ -69,9 +92,30 @@ function App() {
             <input
               type="submit"
               value="Search"
-              className="btn btn-primary col-12"
+              className="btn btn-info col-12"
             />
           </form>
+        </div>
+      </div>
+      {/*  */}
+      <div className="row text-center mb-2">
+        <div className="col-6 mt-2">
+          <button
+            className="btn btn-info"
+            name="decrement"
+            onClick={nextPrevPage}
+          >
+            Prev Page
+          </button>
+        </div>
+        <div className="col-6 mt-2">
+          <button
+            className="btn btn-info"
+            name="increment"
+            onClick={nextPrevPage}
+          >
+            Next Page
+          </button>
         </div>
       </div>
       <div className="row d-flex flex-row flex-wrap justify-content-center m-auto">
@@ -86,6 +130,11 @@ function App() {
             ))
           : null}
       </div>
+      {/* <div className="row footer bg-info">
+        <div className="col-12  p-4 text-white text-center">
+          <p>Designed by Nooruldeen82</p>
+        </div>
+      </div> */}
     </div>
   );
 }
